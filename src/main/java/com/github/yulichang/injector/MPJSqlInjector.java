afc9a528e406ba6b8083067e26d708cbcd87bebd
admin <570810310@qq.com>
2021-04-29 13:38:38 +0800

    优化

--- src/main/java/com/github/yulichang/injector/MPJSqlInjector.java
+++ src/main/java/com/github/yulichang/injector/MPJSqlInjector.java
@@ -3,6 +3,8 @@ package com.github.yulichang.injector;
 import com.baomidou.mybatisplus.core.injector.AbstractMethod;
 import com.baomidou.mybatisplus.core.injector.DefaultSqlInjector;
 import com.github.yulichang.method.*;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 
 import java.util.List;
 
@@ -12,6 +14,7 @@ import java.util.List;
  * @author yulichang
  * @see DefaultSqlInjector
  */
+@ConditionalOnMissingBean(DefaultSqlInjector.class)
 public class MPJSqlInjector extends DefaultSqlInjector {
 
     @Override

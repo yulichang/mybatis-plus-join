8f1ef267c47884418f34aafdeebabc70b160e8fe
admin <570810310@qq.com>
2021-05-24 10:16:57 +0800

    https://gitee.com/baomidou/mybatis-plus/issues/I3SE8R

--- src/main/java/com/github/yulichang/base/MPJBaseServiceImpl.java
+++ src/main/java/com/github/yulichang/base/MPJBaseServiceImpl.java
@@ -3,6 +3,7 @@ package com.github.yulichang.base;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.github.yulichang.interfaces.MPJBaseJoin;
+import com.github.yulichang.toolkit.ReflectionKit;
 
 import java.util.List;
 import java.util.Map;
@@ -11,7 +12,32 @@ import java.util.Map;
  * @author yulichang
  * @see ServiceImpl
  */
+@SuppressWarnings("unchecked")
 public class MPJBaseServiceImpl<M extends MPJBaseMapper<T>, T> extends ServiceImpl<M, T> implements MPJBaseService<T> {
+
+    /**
+     * mybatis plus 3.4.3 bug
+     * <p>
+     * https://gitee.com/baomidou/mybatis-plus/issues/I3SE8R
+     * <p>
+     * https://gitee.com/baomidou/mybatis-plus/commit/7210b461b23211e6b95ca6de2d846aa392bdc28c
+     */
+    @Override
+    protected Class<T> currentMapperClass() {
+        return (Class<T>) ReflectionKit.getSuperClassGenericType(this.getClass(), ServiceImpl.class, 0);
+    }
+
+    /**
+     * mybatis plus 3.4.3 bug
+     * <p>
+     * https://gitee.com/baomidou/mybatis-plus/issues/I3SE8R
+     * <p>
+     * https://gitee.com/baomidou/mybatis-plus/commit/7210b461b23211e6b95ca6de2d846aa392bdc28c
+     */
+    @Override
+    protected Class<T> currentModelClass() {
+        return (Class<T>) ReflectionKit.getSuperClassGenericType(this.getClass(), ServiceImpl.class, 1);
+    }
 
     @Override
     public <DTO> DTO selectJoinOne(Class<DTO> clazz, MPJBaseJoin wrapper) {
--- src/main/java/com/github/yulichang/injector/MPJSqlInjector.java
+++ src/main/java/com/github/yulichang/injector/MPJSqlInjector.java
@@ -2,10 +2,20 @@ package com.github.yulichang.injector;
 
 import com.baomidou.mybatisplus.core.injector.AbstractMethod;
 import com.baomidou.mybatisplus.core.injector.DefaultSqlInjector;
+import com.baomidou.mybatisplus.core.mapper.Mapper;
+import com.baomidou.mybatisplus.core.metadata.TableInfo;
+import com.baomidou.mybatisplus.core.metadata.TableInfoHelper;
+import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
+import com.baomidou.mybatisplus.core.toolkit.GlobalConfigUtils;
 import com.github.yulichang.method.*;
+import com.github.yulichang.toolkit.ReflectionKit;
+import org.apache.ibatis.builder.MapperBuilderAssistant;
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * SQL 注入器
@@ -15,6 +25,7 @@ import java.util.List;
  */
 @ConditionalOnMissingBean(DefaultSqlInjector.class)
 public class MPJSqlInjector extends DefaultSqlInjector {
+    private static final Log logger = LogFactory.getLog(MPJSqlInjector.class);
 
     @Override
     public List<AbstractMethod> getMethodList(Class<?> mapperClass) {
@@ -26,5 +37,32 @@ public class MPJSqlInjector extends DefaultSqlInjector {
         list.add(new SelectJoinMaps());
         list.add(new SelectJoinMapsPage());
         return list;
+    }
+
+    /**
+     * mybatis plus 3.4.3 bug
+     * <p>
+     * https://gitee.com/baomidou/mybatis-plus/issues/I3SE8R
+     * <p>
+     * https://gitee.com/baomidou/mybatis-plus/commit/7210b461b23211e6b95ca6de2d846aa392bdc28c
+     */
+    @Override
+    public void inspectInject(MapperBuilderAssistant builderAssistant, Class<?> mapperClass) {
+        Class<?> modelClass = ReflectionKit.getSuperClassGenericType(mapperClass, Mapper.class, 0);
+        if (modelClass != null) {
+            String className = mapperClass.toString();
+            Set<String> mapperRegistryCache = GlobalConfigUtils.getMapperRegistryCache(builderAssistant.getConfiguration());
+            if (!mapperRegistryCache.contains(className)) {
+                List<AbstractMethod> methodList = this.getMethodList(mapperClass);
+                if (CollectionUtils.isNotEmpty(methodList)) {
+                    TableInfo tableInfo = TableInfoHelper.initTableInfo(builderAssistant, modelClass);
+                    // 循环注入自定义方法
+                    methodList.forEach(m -> m.inject(builderAssistant, mapperClass, modelClass, tableInfo));
+                } else {
+                    logger.debug(mapperClass.toString() + ", No effective injection method was found.");
+                }
+                mapperRegistryCache.add(className);
+            }
+        }
     }
 }

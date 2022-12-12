8d4dbcaec8fe92eb28214713f9c29024037b0cec
yulichang <570810310@qq.com>
2022-12-12 18:39:23 +0800

    test

--- mybatis-plus-join-test/test-join/src/main/java/com/github/yulichang/test/join/mapper/AreaMapper.java
+++ mybatis-plus-join-test/test-join/src/main/java/com/github/yulichang/test/join/mapper/AreaMapper.java
@@ -1,10 +1,9 @@
 package com.github.yulichang.test.join.mapper;
 
-import com.github.yulichang.base.MPJBaseMapper;
 import com.github.yulichang.test.join.entity.AreaDO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 @SuppressWarnings("unused")
-public interface AreaMapper extends MPJBaseMapper<AreaDO> {
+public interface AreaMapper extends MyBaseMapper<AreaDO> {
 }
--- mybatis-plus-join-test/test-join/src/main/java/com/github/yulichang/test/join/mapper/MyBaseMapper.java
+++ mybatis-plus-join-test/test-join/src/main/java/com/github/yulichang/test/join/mapper/MyBaseMapper.java
@@ -3,7 +3,11 @@ package com.github.yulichang.test.join.mapper;
 import com.github.yulichang.base.MPJBaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 @SuppressWarnings("unused")
-public interface MYBaseMapper<T> extends MPJBaseMapper<T> {
+public interface MyBaseMapper<T> extends MPJBaseMapper<T> {
+
+    int insertBatchSomeColumn(List<T> entityList);
 }

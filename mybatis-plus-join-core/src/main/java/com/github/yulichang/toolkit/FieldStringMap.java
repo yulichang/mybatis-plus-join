b8fbc6fc04b6f25876be0410d2662db113d49dab
yulichang <570810310@qq.com>
2023-12-12 19:39:00 +0800

    fix https://gitee.com/best_handsome/mybatis-plus-join/issues/I8NTF1

--- mybatis-plus-join-core/src/main/java/com/github/yulichang/toolkit/FieldStringMap.java
+++ mybatis-plus-join-core/src/main/java/com/github/yulichang/toolkit/FieldStringMap.java
@@ -12,11 +12,11 @@ public class FieldStringMap<V> extends HashMap<String, V> {
 
     @Override
     public V get(Object key) {
-        return super.get(((String) key).toUpperCase(Locale.ENGLISH));
+        return super.get(key == null ? null : ((String) key).toUpperCase(Locale.ENGLISH));
     }
 
     @Override
     public boolean containsKey(Object key) {
-        return super.containsKey(((String) key).toUpperCase(Locale.ENGLISH));
+        return super.containsKey(key == null ? null : ((String) key).toUpperCase(Locale.ENGLISH));
     }
 }

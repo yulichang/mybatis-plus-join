2838731d29a04c2e39c24a714db5ec9c6d1bc3a7
yulichang <570810310@qq.com>
2024-06-01 07:00:34 +0800

    1.4.13

--- mybatis-plus-join-test/test-join/src/test/java/com/github/yulichang/test/join/unit/ApplyFuncTest.java
+++ mybatis-plus-join-test/test-join/src/test/java/com/github/yulichang/test/join/unit/ApplyFuncTest.java
@@ -39,6 +39,22 @@ public class ApplyFuncTest {
                 .list();
 
         list.forEach(System.out::println);
+
+        ThreadLocalUtils.set("SELECT t.pid, t.`name`, t.`json`, t.sex, t.head_img, t.create_time, t.address_id, " +
+                "t.address_id2, t.del, t.create_by, t.update_by FROM `user` t " +
+                "LEFT JOIN address t1 ON (t1.user_id = t.id) WHERE t.del = false AND t1.del = false " +
+                "AND (concat(t.id, t1.user_id, ?) IS NOT NULL " +
+                "AND concat(t.id, t1.user_id, ?) IS NOT NULL)");
+        List<UserDO> list1 = JoinWrappers.lambda(UserDO.class)
+                .selectAll(UserDO.class, UserDO::getId)
+                .leftJoin(AddressDO.class, AddressDO::getUserId, UserDO::getId)
+                .applyFunc("concat(%s,%s,{0}) is not null", arg -> arg.accept(UserDO::getId, AddressDO::getUserId), "12")
+                .applyFunc("concat(%s,%s,{0}) is not null", arg -> arg.accept(
+                        Fun.f("t", UserDO::getId),
+                        Fun.f("t1", AddressDO::getUserId)), "12")
+                .list();
+
+        list1.forEach(System.out::println);
     }
 
 }

2fcd175d92975332275f0556a2dc17117e0f2108
yulichang <570810310@qq.com>
2026-06-01 15:00:44 +0800

    Revert "调整"
    This reverts commit 616d84619701a40f18db44234af4829b4af55a73.

--- mybatis-plus-join-core/src/main/java/com/github/yulichang/adapter/IAdapter.java
+++ mybatis-plus-join-core/src/main/java/com/github/yulichang/adapter/IAdapter.java
@@ -20,6 +20,7 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 /**
